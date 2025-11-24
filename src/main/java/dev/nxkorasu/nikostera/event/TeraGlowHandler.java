2de713408c1db41e49c49c5a7d7d20773037856d
Relahx <rhunterkyt@gmail.com>
2025-11-24 15:25:05 -0300

    Update TeraGlowHandler.java

--- src/main/java/dev/nxkorasu/nikostera/event/TeraGlowHandler.java
+++ src/main/java/dev/nxkorasu/nikostera/event/TeraGlowHandler.java
@@ -4,12 +4,13 @@ import com.cobblemon.mod.common.api.events.battles.BattleFledEvent;
 import com.cobblemon.mod.common.api.events.battles.BattleVictoryEvent;
 import com.cobblemon.mod.common.api.events.battles.instruction.TerastallizationEvent;
 import com.cobblemon.mod.common.api.events.pokemon.PokemonCapturedEvent;
-import com.cobblemon.mod.common.api.events.pokemon.PokemonSentPostEvent;
+import com.cobblemon.mod.common.api.events.pokemon.PokemonSentEvent;
 import com.cobblemon.mod.common.api.pokemon.feature.FlagSpeciesFeature;
 import com.cobblemon.mod.common.api.pokemon.feature.StringSpeciesFeature;
 import com.cobblemon.mod.common.api.types.tera.TeraType;
 import com.cobblemon.mod.common.api.types.tera.TeraTypes;
 import com.cobblemon.mod.common.battles.pokemon.BattlePokemon;
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
 import com.cobblemon.mod.common.pokemon.Pokemon;
 import kotlin.Unit;
 import net.minecraft.entity.LivingEntity;
@@ -26,123 +27,101 @@ import java.util.List;
 import java.util.UUID;
 
 public class TeraGlowHandler {
-    public static Unit terrastallizationUsed(TerastallizationEvent terastallizationEvent) {
-        //Obtain the Entity and Pokemon Affected
-        LivingEntity pokemon = terastallizationEvent.getPokemon().getEffectedPokemon().getEntity();
-        Pokemon pk = terastallizationEvent.getPokemon().getEffectedPokemon();
-        //Change the pokemon if needed
-        if (pk.getSpecies().getName().equals("Terapagos")) {
+    public static Unit terrastallizationUsed(TerastallizationEvent event) {
+        LivingEntity pokemon = event.getPokemon().getEffectedPokemon().getEntity();
+        Pokemon pk = event.getPokemon().getEffectedPokemon();
+        if (pk.getSpecies().getName().equals("Terapagos"))
             new StringSpeciesFeature("tera_form", "stellar").apply(pk);
-        }
-        if (pk.getSpecies().getName().equals("Ogerpon")) {
+        if (pk.getSpecies().getName().equals("Ogerpon"))
             new FlagSpeciesFeature("embody_aspect", true).apply(pk);
-        }
-        //Obtain the Pokemon´s World, create a team and give it a color though formatting
         if (pokemon.getWorld() instanceof ServerWorld serverLevel) {
             ServerScoreboard scoreboard = serverLevel.getScoreboard();
             String teamName = "glow_" + UUID.randomUUID().toString().substring(0, 8);
-
             Team team = scoreboard.getTeam(teamName);
-
-            Formatting color = getGlowColorForTeraType(terastallizationEvent.getPokemon().getEffectedPokemon().getTeraType());
+            Formatting color = getGlowColorForTeraType(pk.getTeraType());
             if (team == null) {
                 team = scoreboard.addTeam(teamName);
                 team.setColor(color);
             }
             scoreboard.addScoreHolderToTeam(pokemon.getUuid().toString(), team);
-            //Give the pokemon a barrier as last flower fed for control reasons
             pk.setLastFlowerFed(new ItemStack(Items.BARRIER, 1));
-            //Apply the glowing effect to the Entity
             pokemon.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, Integer.MAX_VALUE, 0));
         }
         return Unit.INSTANCE;
     }
-    //Determines what color will the team have depending on the pokemon´s teratype
-    public static Formatting getGlowColorForTeraType(TeraType teratype) {
-        Formatting teratypeColor;
-        switch (teratype.getDisplayName().getString().toLowerCase()) {
-            case "fire" -> teratypeColor = Formatting.RED;
-            case "water" -> teratypeColor = Formatting.BLUE;
-            case "electric" -> teratypeColor = Formatting.YELLOW;
-            case "grass" -> teratypeColor = Formatting.GREEN;
-            case "ice", "flying" -> teratypeColor = Formatting.AQUA;
-            case "fighting" -> teratypeColor = Formatting.DARK_RED;
-            case "poison" -> teratypeColor = Formatting.DARK_PURPLE;
-            case "ground" -> teratypeColor = Formatting.GOLD;
-            case "psychic", "fairy" -> teratypeColor = Formatting.LIGHT_PURPLE;
-            case "bug" -> teratypeColor = Formatting.DARK_GREEN;
-            case "rock" -> teratypeColor = Formatting.GRAY;
-            case "ghost" -> teratypeColor = Formatting.DARK_GRAY;
-            case "dragon" -> teratypeColor = Formatting.DARK_BLUE;
-            case "dark" -> teratypeColor = Formatting.BLACK;
-            case "steel" -> teratypeColor = Formatting.DARK_AQUA;
-            default -> teratypeColor = Formatting.WHITE;
-        }
-        return teratypeColor;
+
+    public static Formatting getGlowColorForTeraType(TeraType type) {
+        return switch (type.getDisplayName().getString().toLowerCase()) {
+            case "fire" -> Formatting.RED;
+            case "water" -> Formatting.BLUE;
+            case "electric" -> Formatting.YELLOW;
+            case "grass" -> Formatting.GREEN;
+            case "ice", "flying" -> Formatting.AQUA;
+            case "fighting" -> Formatting.DARK_RED;
+            case "poison" -> Formatting.DARK_PURPLE;
+            case "ground" -> Formatting.GOLD;
+            case "psychic", "fairy" -> Formatting.LIGHT_PURPLE;
+            case "bug" -> Formatting.DARK_GREEN;
+            case "rock" -> Formatting.GRAY;
+            case "ghost" -> Formatting.DARK_GRAY;
+            case "dragon" -> Formatting.DARK_BLUE;
+            case "dark" -> Formatting.BLACK;
+            case "steel" -> Formatting.DARK_AQUA;
+            default -> Formatting.WHITE;
+        };
     }
 
-    public static Unit switchIn(PokemonSentPostEvent pokemonSentPostEvent) {
-        LivingEntity pokemon = pokemonSentPostEvent.getPokemon().getEntity();
-        Pokemon pk = pokemonSentPostEvent.getPokemon();
-        //If the pokemon has a barrier on its last flower fed slot, give it glow
-        if (pokemonSentPostEvent.getPokemon().getLastFlowerFed().getItem().equals(Items.BARRIER)) {
+    public static Unit switchIn(PokemonSentEvent.Post event) {
+        Pokemon pk = event.getPokemon();
+        PokemonEntity pokemonEntity = event.getPokemonEntity();
+        LivingEntity pokemon = pokemonEntity;
+        if (pk.getLastFlowerFed().getItem().equals(Items.BARRIER)) {
             if (pokemon.getWorld() instanceof ServerWorld serverLevel) {
                 ServerScoreboard scoreboard = serverLevel.getScoreboard();
                 String teamName = "glow_" + UUID.randomUUID().toString().substring(0, 8);
-
                 Team team = scoreboard.getTeam(teamName);
-
-                Formatting color = getGlowColorForTeraType(pokemonSentPostEvent.getPokemon().getTeraType());
+                Formatting color = getGlowColorForTeraType(pk.getTeraType());
                 if (team == null) {
                     team = scoreboard.addTeam(teamName);
                     team.setColor(color);
                 }
                 scoreboard.addScoreHolderToTeam(pokemon.getUuid().toString(), team);
-                if (pk.getSpecies().getName().equalsIgnoreCase("terapagos")) {
+                if (pk.getSpecies().getName().equalsIgnoreCase("terapagos"))
                     new StringSpeciesFeature("tera_form", "terastal").apply(pk);
-                }
-                if (pk.getSpecies().getName().equalsIgnoreCase("ogerpon")) {
+                if (pk.getSpecies().getName().equalsIgnoreCase("ogerpon"))
                     new FlagSpeciesFeature("embody_aspect", false).apply(pk);
-                }
                 pokemon.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, Integer.MAX_VALUE, 0));
             }
         }
         return Unit.INSTANCE;
     }
 
-    public static Unit leaveBattle(BattleFledEvent battleFledEvent) {
-        //clear the lastflowerfed on fleed
-        clearFlowers(battleFledEvent.getPlayer().getPokemonList());
-        return Unit.INSTANCE;
-    }
-    public static void clearFlowers(List<BattlePokemon> team){
-        //Just clear the last fed flower slot for each pokemon
-        for (BattlePokemon pokemon : team) {
-            pokemon.getEffectedPokemon().setLastFlowerFed(new ItemStack(Items.AIR,1));
-            if (pokemon.getEffectedPokemon().getSpecies().getName().equals("Terapagos")) {
-                new StringSpeciesFeature("tera_form", "terastal").apply(pokemon.getOriginalPokemon());
-            }
-            if (pokemon.getEffectedPokemon().getSpecies().getName().equals("Ogerpon")) {
-                new FlagSpeciesFeature("embody_aspect", false).apply(pokemon.getOriginalPokemon());
-            }
-        }
-    }
-
-    public static Unit winBattle(BattleVictoryEvent battleVictoryEvent) {
-        //clear the lastflowerfed when a battle ends, on both sides
-        clearFlowers(battleVictoryEvent.getWinners().getFirst().getPokemonList());
-        clearFlowers(battleVictoryEvent.getLosers().getFirst().getPokemonList());
+    public static Unit leaveBattle(BattleFledEvent event) {
+        clearFlowers(event.getPlayer().getPokemonList());
         return Unit.INSTANCE;
     }
 
-    public static Unit capturedPokemon(PokemonCapturedEvent pokemonCapturedEvent) {
-        //if terapagos or ogerpon are captured, set their apropied teratypes
-        if(pokemonCapturedEvent.getPokemon().getSpecies().getName().equalsIgnoreCase("ogerpon")){
-            pokemonCapturedEvent.getPokemon().setTeraType(TeraTypes.getGRASS());
+    public static void clearFlowers(List<BattlePokemon> team) {
+        for (BattlePokemon bp : team) {
+            bp.getEffectedPokemon().setLastFlowerFed(new ItemStack(Items.AIR, 1));
+            if (bp.getEffectedPokemon().getSpecies().getName().equals("Terapagos"))
+                new StringSpeciesFeature("tera_form", "terastal").apply(bp.getOriginalPokemon());
+            if (bp.getEffectedPokemon().getSpecies().getName().equals("Ogerpon"))
+                new FlagSpeciesFeature("embody_aspect", false).apply(bp.getOriginalPokemon());
         }
-        if(pokemonCapturedEvent.getPokemon().getSpecies().getName().equalsIgnoreCase("terapagos")){
-            pokemonCapturedEvent.getPokemon().setTeraType(TeraTypes.getSTELLAR());
-        }
+    }
+
+    public static Unit winBattle(BattleVictoryEvent event) {
+        clearFlowers(event.getWinners().getFirst().getPokemonList());
+        clearFlowers(event.getLosers().getFirst().getPokemonList());
+        return Unit.INSTANCE;
+    }
+
+    public static Unit capturedPokemon(PokemonCapturedEvent event) {
+        if (event.getPokemon().getSpecies().getName().equalsIgnoreCase("ogerpon"))
+            event.getPokemon().setTeraType(TeraTypes.getGRASS());
+        if (event.getPokemon().getSpecies().getName().equalsIgnoreCase("terapagos"))
+            event.getPokemon().setTeraType(TeraTypes.getSTELLAR());
         return Unit.INSTANCE;
     }
 }
